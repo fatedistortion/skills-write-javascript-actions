a19e80959da41cfb73195bc36a544cc43d6dba20
Carlos Hernandez <carlos.hernandez2@petco.com>
2025-10-23 15:50:01 -0600

    remove console.log

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -3,8 +3,7 @@ const core = require("@actions/core");
 
 async function run() {
   const joke = await getJoke();
-  console.log(joke);
-  core.setOutput("joke-output", joke);
+  core.setOutput("joke-text", joke);
 }
 
 run();
